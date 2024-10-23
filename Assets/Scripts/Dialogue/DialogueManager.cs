bcca93f0f3d2990300e403dc78c65f8f35d10263
Amelia Silver <miasilver4@gmail.com>
2024-10-23 00:46:27 -0400

    fixing dialogue canvas

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -7,17 +7,33 @@ using UnityEngine.EventSystems;
 
 
 public class DialogueManager : MonoBehaviour {
+    [Header("Params")]
+    [SerializeField] private float typingSpeed = 0.04f;
+    
+    [Header("Dialogue UI")]
     [SerializeField] GameObject dialoguePanel;
     [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] TextMeshProUGUI displayNameText;
+    private Animator layoutAnimator;
     [SerializeField] GameObject continueIcon;
     private Story currentStory;
     public bool dialogueIsPlaying {get; private set;}
+    private Coroutine displayLineCoroutine;
+    private bool canContinueToNextLine = false;
+
+    [Header("Choices UI")]
     [SerializeField] GameObject[] choices;
     private TextMeshProUGUI[] choicesText;
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+    private const string LAYOUT_TAG = "layout";
 
     void Start() {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
+
+        layoutAnimator = dialoguePanel.GetComponent<Animator>();
 
         choicesText = new TextMeshProUGUI[choices.Length];
         int index = 0;
@@ -28,7 +44,13 @@ public class DialogueManager : MonoBehaviour {
     }
 
     void Update() {
-        if (dialogueIsPlaying && Input.GetKeyDown(KeyCode.Space)) {
+        if (!dialogueIsPlaying) {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) 
+                && canContinueToNextLine
+                && currentStory.currentChoices.Count == 0) {
             ContinuePlaying();
         }
     }
@@ -37,18 +59,24 @@ public class DialogueManager : MonoBehaviour {
         currentStory = new Story(inkJSON.text);
         dialogueIsPlaying = true;
         dialoguePanel.SetActive(true);
-        Time.timeScale = 0;
+        // stop player movement
+
+        displayNameText.text = "???";
+        layoutAnimator.Play("right");
 
         ContinuePlaying();
     }
 
     public void ContinuePlaying() {
          if (currentStory.canContinue) {
-            // set text for current dialogue line
-            dialogueText.text = currentStory.Continue();
+            // set text for the current dialogue line
+            if (displayLineCoroutine != null) {
+                StopCoroutine(displayLineCoroutine);
+            }
+            displayLineCoroutine = StartCoroutine(DisplayLine(currentStory.Continue()));
 
-            // display choices, if any, for this dialogue line
-            DisplayChoices();
+            // handle tags
+            HandleTags(currentStory.currentTags);
 
         }
         else {
@@ -56,11 +84,36 @@ public class DialogueManager : MonoBehaviour {
         }
     }
 
+    private IEnumerator DisplayLine(string line) {
+        //empty the dialogue text
+        dialogueText.text = "";
+
+        // hide items while text is typing
+        continueIcon.SetActive(false);
+        HideChoices();
+
+        canContinueToNextLine = false;
+
+        // display each letter one at a time
+        foreach (char letter in line.ToCharArray()) {
+            // finish writing the line right away
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                dialogueText.text = line;
+                break;
+            }
+
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        // display choices, if any, for this dialogue line
+        DisplayChoices();
+    }
+
     private void ExitDialogueMode() {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
         dialogueText.text = "";
-        Time.timeScale = 1;
+        // start player movement
     }
 
     private void DisplayChoices() {
@@ -70,8 +123,9 @@ public class DialogueManager : MonoBehaviour {
         if (currentChoices.Count > choices.Length) {
             Debug.LogError("More choices were given than the current UI can support.");
         }
-        else if (currentChoices.Count < 0) {
+        else if (currentChoices.Count <= 0) {
             continueIcon.SetActive(true);
+            canContinueToNextLine = true;
             return;
         }
 
@@ -87,12 +141,45 @@ public class DialogueManager : MonoBehaviour {
         for (int i = index; i < choices.Length; i++) {
             choices[i].gameObject.SetActive(false);
         }
+        continueIcon.SetActive(false);
+        canContinueToNextLine = false;
+    }
 
-        continueIcon.SetActive(true);
-
+    public void HideChoices() {
+        foreach (GameObject choiceButton in choices) {
+            choiceButton.SetActive(false);
+        }
     }
 
     public void MakeChoice(int choiceIndex) {
         currentStory.ChooseChoiceIndex(choiceIndex);
+        ContinuePlaying();
+    }
+
+    public void HandleTags(List<string> currentTags) {
+        foreach (string tag in currentTags) {
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length != 2) {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);  
+            }
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey) {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    break;
+                case PORTRAIT_TAG:
+                    Debug.Log("portrait = " + tagValue);
+                    break;
+                case LAYOUT_TAG:
+                    layoutAnimator.Play(tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                    break;
+            }
+        }
     }
 }
